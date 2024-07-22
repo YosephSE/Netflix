e3d567923796dffa1d540bcd53cf6dc7b110efe5
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-22 23:48:31 +0300

    UI with api but not auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
-import Login from "./pages/Login"
-import SingUp from "./pages/SingUp"
-import Account from "./pages/Account"
 
 function App() {
 
@@ -13,9 +10,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/signup" element={<SingUp/>}></Route>
-        <Route path="/account" element={<Account/>}></Route>
       </Routes>
     </>
   )
