326785d206dda02746766beec3b12d4ad6a9b80d
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-22 13:55:41 +0300

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
+import Login from "./pages/Login"
+import SingUp from "./pages/SingUp"
+import Account from "./pages/Account"
 
 function App() {
 
@@ -10,6 +13,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/signup" element={<SingUp/>}></Route>
+        <Route path="/account" element={<Account/>}></Route>
       </Routes>
     </>
   )
