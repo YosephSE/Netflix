9458890b9f84e633a7ab77a72bf533ace4d7e6c0
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-22 14:10:24 +0300

    signin, signup link on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between z-50 absolute w-full p-3'>
-    <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
-    <div>
-    <button className="text-white pr-4">Sign In</button>
-    <button className="bg-red-600 px-4 py-2 rounded">Sign Up</button>
+    <div className="flex items-center justify-between z-50 absolute w-full p-3">
+      <Link to="/">
+        <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+          NETFLIX
+        </h1>
+      </Link>
+      <div>
+        <Link to="/signin">
+          <button className="text-white pr-4">Sign In</button>
+        </Link>
+        <Link to="/signup">
+          <button className="bg-red-600 px-4 py-2 rounded">Sign Up</button>
+        </Link>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Account = () => {
   return (
     <div>
-      
+      Account
     </div>
   )
 }
