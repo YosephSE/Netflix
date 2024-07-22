a33d08f9c2f27d78b6cfbb5a65eecf8a8799228c
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-22 13:30:44 +0300

    firebase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between z-50 absolute w-full'>
+    <div className='flex items-center justify-between z-50 absolute w-full p-3'>
     <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
     <div>
     <button className="text-white pr-4">Sign In</button>
